e2ecc2253d3c52e323a33cab6e8c3578cf03b229
VictorLuis123 <150992051+VictorLuis123@users.noreply.github.com>
2024-04-25 05:17:55 -0400

    Arreglada(parcialmente) la comunicacion entre el form y el php

--- src/app/coach/coach.component.ts
+++ src/app/coach/coach.component.ts
@@ -13,7 +13,16 @@ export class CoachComponent {
   constructor(private dataservice:DataserviceService,private router: Router){}
 
   insertTeam(datos_equipo:{nombre_equipo:string, contrasenna_equipo:string,nombre_p1:string,id_p1:number,edad_p1:number, sexo_p1:string, pais_p1:string, nombre_p2:string,id_p2:number,edad_p2:number, sexo_p2:string, pais_p2:string,  nombre_p3:string,id_p3:number,edad_p3:number, sexo_p3:string, pais_p3:string}){
-    this.dataservice.insertarEquipoRequest(datos_equipo).subscribe();
+    this.dataservice.insertarEquipoRequest(datos_equipo).subscribe(
+      (response:any) => {
+            console.log(response);
+            if(response[0].mensaje=='Equipo insertado correctamente'){
+              alert('Se insertoooooooo')
+            }else{
+            alert('No puedesss')
+           }
+          }
+    );
 
     // this.dataservice.comprobarLoggin(team_data.nombre,team_data.contrasenna).subscribe(
     //   (response:any) => {
